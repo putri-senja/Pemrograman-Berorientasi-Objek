d18166ea6adc83cca638aac3d69e04214a409cc1
Putri Senja <ptrsenjana30@gmail.com>
2026-09-15 08:06:06 +0000

    Add files via upload

---
+++ putri_project1/src/main/java/com/mycompany/putri_project1/Putri_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.putri_project1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Putri_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama    : Putri Senja Nuril Azizah");
+        System.out.println("NPM     : 2505060036");
+        System.out.println("Alamat  : Kota Magelang");
+        System.out.println("No_HP   : 087725994829");
+    }
+}
